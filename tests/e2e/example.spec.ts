596daa5c163ca0501d48b5d793cb3222831f0750
Nicky Semenza <nicky@nickysemenza.com>
2025-02-07 15:04:37 -0800

    playwright test

---
+++ tests/e2e/example.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/RecipeHub/);
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "recipes" }).click();
+});
