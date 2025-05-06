e8921596eb09a0875bdacedda4f41e8be6b2fe60
Nicky Semenza <nicky@nickysemenza.com>
2025-05-06 13:57:43 -0700

    add back location detail breadcrumb

--- src/app/locations/[id]/page.tsx
+++ src/app/locations/[id]/page.tsx
@@ -1,6 +1,15 @@
 import { api } from "~/trpc/server";
 import { LocationDetail } from "~/app/_components/locations/location-detail";
 import { WasmContextProvider } from "~/wasmContext";
+import {
+  BreadcrumbSeparator,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Breadcrumb,
+  BreadcrumbList,
+} from "~/components/ui/breadcrumb";
+import { collectInfiniteParents } from "~/schemas/location";
+import Link from "next/link";
 
 type DetailParams = { id: string };
 type PageParams = { params: Promise<DetailParams> };
@@ -15,12 +24,39 @@ export async function generateMetadata({ params }: PageParams) {
 
 export default async function Page({ params }: PageParams) {
   const id = (await params).id;
-  const location = await api.location.getByID({
-    id,
-  });
+  const location = await api.location.getByID({ id });
+
   return (
-    <WasmContextProvider>
-      <LocationDetail location={location} />
-    </WasmContextProvider>
+    <div>
+      <Breadcrumb className="p-6">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          {collectInfiniteParents(location)
+            .reverse()
+            .map((parentLocation) => (
+              <>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem key={parentLocation.id}>
+                  <BreadcrumbLink asChild>
+                    <Link href={`/locations/${parentLocation.id}`}>
+                      {parentLocation.name}
+                    </Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              </>
+            ))}
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>{location.name}</BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <WasmContextProvider>
+        <LocationDetail location={location} />
+      </WasmContextProvider>
+    </div>
   );
 }
