4ad7a0e795a07d1daae2b2e289b765e3243c0dd4
Nicky Semenza <nicky@nickysemenza.com>
2025-09-09 17:36:50 -0700

    update indices

--- src/app/homepage/entitycount.tsx
+++ src/app/homepage/entitycount.tsx
@@ -10,7 +10,7 @@ export default function EntityCount() {
   const opts = {
     filters: {},
     sort,
-    pagination: { pageIndex: 0, pageSize: 100 },
+    pagination: { pageIndex: 0, pageSize: 0 },
   };
 
   const [location, product, ingredient, recipe, inventoryItem, usda, image] =
